a591ede067f2c25f51e0c10dbbb7ceb7a81fbf7c
nbspavankalyan <53966061+nbspavankalyan@users.noreply.github.com>
2019-08-10 13:55:36 +0530

    Add files via upload

---
+++ japancoffee.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num,sum=0;
+float per;
+cin>>num>>per;
+while(n!=0)
+{
+sum=sum+num;
+num=num-num*(per/100);
+}
+cout<<sum;
+return 0;
+}
